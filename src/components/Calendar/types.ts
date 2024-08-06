b57cd0abf63cc1608bde8083e5222c20c94e234b
divineDuckk <assasinsmn@gmail.com>
2024-08-06 15:00:10 +0300

    feat: add date-picker interface

--- src/components/Calendar/types.ts
+++ src/components/Calendar/types.ts
@@ -1,12 +1,11 @@
 import { startDay } from '@/interfaces';
+import { DatePickerProps } from '@/components/DatePicker/types';
 
-export interface CalendarProps {
+export interface CalendarProps extends DatePickerProps {
   startYear: number;
   endYear: number;
   weekStartDay: startDay;
   withExtraDays: boolean;
   withWeekends: boolean;
   withHolidays: boolean;
-  handlePickDay?: (fullDate: string) => () => void;
-  startDate?: string;
 }
